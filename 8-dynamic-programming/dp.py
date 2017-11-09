765cffebaadf78581c5fa18af67085468d8d389a
Colin Clement <colin.clement@gmail.com>
2017-11-08 22:28:40 -0500

    edit distance and other problems

--- 8-dynamic-programming/dp.py
+++ 8-dynamic-programming/dp.py
@@ -48,3 +48,128 @@ def binomial(n, m):
         for j in range(1, i):
             p[i][j] = p[i - 1][j - 1] + p[i - 1][j]
     return p[n][m]
+
+def match(s, t):
+    return 1 - (s == t)
+
+def indel(s):
+    """ if there is a space, either an insertion or deletion is required """
+    return 1
+
+def string_compare(s, t, i, j):
+    if i < 0:  # no more of P to match
+        return j * indel(" ")
+    if j < 0:  # no more of T to edit
+        return i * indel(" ")
+
+    matched = edit_distance(s, t, i-1, j-1) + match(s[i], t[j])
+    insert = edit_distance(s, t, i, j-1) + indel(t[j])
+    delete = edit_distance(s, t, i-1, j) + indel(s[i])
+
+    return min(matched, insert, delete)
+
+from collections import namedtuple
+
+Cell = namedtuple('Cell', ['cost', 'parent'])
+
+
+class EditDistance(object):
+    def match(self, s, t):
+        return 1 - (s == t)
+    
+    def indel(self, s):
+        """ if there is a space, either an insertion or deletion is required """
+        return 1
+
+    def row_init(self, i, m):
+        for i in range(len(m)):
+            m[i][0] = Cell(i, -1 if not i else 2)  # all deletes
+    
+    def col_init(self, j, m):
+        for j in range(len(m[0])):
+            m[0][j] = Cell(j, -1 if not j else 1)  # all inserts
+
+    def goal_cell(self, s, t, m):
+        return len(s) - 1, len(t) - 1
+
+    def string_compare(self, s, t):
+        if not s[0] == " ":
+            s = " " + s
+        if not t[0] == " ":
+            t = " " + t
+        
+        maxlen = len(s) + len(t)
+        m = [[None for i in range(len(t))] for j in
+             range(len(s))]
+        opt = [maxlen] * 3
+        # Set boundary conditions, matching empty string
+        self.row_init(i, m)
+        self.col_init(j, m)
+    
+        for i in range(1, len(s)):
+            for j in range(1, len(t)):
+                opt[0] = m[i - 1][j - 1].cost + self.match(s[i], t[j])
+                opt[1] = m[i][j - 1].cost + self.indel(t[j])
+                opt[2] = m[i - 1][j].cost + self.indel(s[i])
+    
+                argsort = sorted(range(3), key = lambda i: opt[i])
+                m[i][j] = Cell(opt[argsort[0]], argsort[0])
+        
+        i, j = self.goal_cell(s, t, m)
+        return m[i][j].cost, m
+    
+    @staticmethod
+    def match_out(s, t, i, j, path):
+        if s[i] == t[j]:
+            path.append("M")
+        else:
+            path.append("S")
+   
+    @staticmethod
+    def insert_out(t, j, path):
+        path.append("I")
+    
+    @staticmethod
+    def delete_out(s, i, path):
+        path.append("D")
+    
+    @staticmethod
+    def reconstruct_path(m, s, t, i, j, path):
+        if m[i][j].parent < 0:
+            return
+        if m[i][j].parent == 0:  # parent matched
+            EditDistance.reconstruct_path(m, s, t, i - 1, j - 1, path)
+            EditDistance.match_out(s, t, i-1, j-1, path)
+            return
+        if m[i][j].parent == 1:  # parent inserted
+            EditDistance.reconstruct_path(m, s, t, i, j - 1, path)
+            EditDistance.insert_out(t, j-1, path)
+            return
+        if m[i][j].parent == 2:  # parent deleted
+            EditDistance.reconstruct_path(m, s, t, i - 1, j, path)
+            EditDistance.delete_out(s, i-1, path)
+            return
+
+
+class SubstringMatch(EditDistance):
+    def col_init(self, j, m):
+        """ Don't penalize deleting a bunch of T to find close s """ 
+        for j in range(len(m[0])):
+            m[0][j] = Cell(0, -1)
+
+    def goal_cell(self, s, t, m):
+        """ Find part of m for full match of s and best match of t """
+        i = len(s) - 1
+        j =  sorted(range(len(m[0])), key=lambda j: m[i][j].cost)[0]
+        return i, j
+
+
+class LargestCommonSubsequence(EditDistance):
+    def match(self, s, t):
+        """ Substitute must be more costly than delete+insert """
+        return 3 * (not s == t)
+
+
+
+
+
